376f74a6813d8a951b7e5482e64951062da6c5ac
Kamve Gwijana <gwijgwij011@gmail.com>
2023-03-11 11:40:33 +0200

    updates

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -2,14 +2,16 @@ extends Node2D
 
 func _ready():
 	GameManager.player = $Ball
-
-func _process(_delta):
-	$VectorCreator.position = $Ball.position
 	
-	if GameManager.scene_level == 1:
+	if GameManager.scene_level == 4:
 		var dialog = Dialogic.start("EnterAngryRed")
 		add_child(dialog)
 		dialog.connect("timeline_end", self, "after_dialog")
+		get_node("npcCam").set_current(true)
+		
+func _process(_delta):
+	$VectorCreator.position = $Ball.position
 
 func _after_dialog():
 	print("Introduced AngryRed")
+	get_node("npcCam").set_current(false)
